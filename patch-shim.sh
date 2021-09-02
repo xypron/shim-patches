f60af9c9a313930b54f324a89921d018a6731760
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2021-09-02 13:42:52 +0200

    0001-shim-avoid-BOOTx64.EFI-in-message-on-other-architect.patch
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patch-shim.sh
+++ patch-shim.sh
@@ -7,6 +7,7 @@ git am --abort || true
 git am ../patches/0001-Correct-signedness-when-calling-string-functions.patch
 git am ../patches/0002-Initial-RISC-V-support.patch
 
+git am ../patches/0001-shim-avoid-BOOTx64.EFI-in-message-on-other-architect.patch
 git am ../patches/0001-str-remove-duplicate-parameter-check.patch
 git am ../patches/0001-fallback-incorrect-check-after-AllocateZeroPool.patch
 git am ../patches/0001-csv-detect-end-of-csv-file-correctly.patch
