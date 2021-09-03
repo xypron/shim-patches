4b87806badfbde7c537d40c532a653a6e9377835
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2021-09-03 02:00:51 +0200

    0001-test-csv-test-handling-of-trailing-NUL-byte.patch
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- patch-shim.sh
+++ patch-shim.sh
@@ -10,5 +10,6 @@ git am ../patches/0002-Initial-RISC-V-support.patch
 git am ../patches/0001-shim-avoid-BOOTx64.EFI-in-message-on-other-architect.patch
 git am ../patches/0001-str-remove-duplicate-parameter-check.patch
 git am ../patches/0001-fallback-incorrect-check-after-AllocateZeroPool.patch
+git am ../patches/0001-test-csv-test-handling-of-trailing-NUL-byte.patch
 git am ../patches/0001-csv-detect-end-of-csv-file-correctly.patch
 git am ../patches/0001-sbat-append-NUL-byte-to-.sbat-section.patch
